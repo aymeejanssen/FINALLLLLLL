7c4935f2ca18272d3dff1a06cedf15f0d31af472
Aymee Janssen <aymeejanssen@Aymees-MacBook-Air.local>
2025-07-06 14:18:56 +0400

    Fix styles, font, and layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./src/main.js" // <-- this is key
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+      },
       colors: {
-        peach: { 100: '#fff5f0' },
-        lavender: { 100: '#f5f3ff' },
-        yellow: { 100: '#fffce0' },
+        peach: {
+          100: '#fff5f0',
+        },
+        lavender: {
+          100: '#f5f3ff',
+        },
+        yellow: {
+          100: '#fffce0',
+        },
       },
     },
   },
   plugins: [],
-};
+}
